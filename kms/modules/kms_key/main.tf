0b1e6e8329058c1a3aee540e7bb87ad4364ffa16
Ashwini-Kanagaraj <159413670+Ashwini-Kanagaraj@users.noreply.github.com>
2024-04-08 13:49:55 +0530

    Update main.tf

--- kms/modules/kms_key/main.tf
+++ kms/modules/kms_key/main.tf
@@ -1,9 +1,12 @@
+# Creates an AWS KMS key
 resource "aws_kms_key" "my_kms_key" {
   description             = var.description
   deletion_window_in_days = var.deletion_window_in_days
   enable_key_rotation     = var.enable_key_rotation
 
 }
+
+# Creates an AWS KMS alias name
 resource "aws_kms_alias" "my_alias" {
   name          = "alias/${var.aliases_name}"
   target_key_id = aws_kms_key.my_kms_key.arn
