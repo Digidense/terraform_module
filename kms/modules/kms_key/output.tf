19f72b2d7255ccaf906ea5f5627595a30af20352
Ashwini-Kanagaraj <159413670+Ashwini-Kanagaraj@users.noreply.github.com>
2024-04-12 11:26:50 +0530

    Delete kms directory

--- kms/modules/kms_key/output.tf
+++
@@ -1,9 +0,0 @@
-output "key_id" {
-  description = "The ID of the created KMS key."
-  value       = aws_kms_key.my_kms_key.id
-}
-
-output "arn" {
-  description = "The ARN of the created KMS key."
-  value       = aws_kms_key.my_kms_key.id
-}
