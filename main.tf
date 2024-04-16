5231e777dd56ca78fb9fea54177f0b2222edfdcc
Anishadevops <123820506+Anishadevops@users.noreply.github.com>
2024-04-16 14:29:50 +0530

    ECR module creation

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+// Module Block: KMS Module
 module "Kms_module" {
   source                  = "git::https://github.com/Digidense/terraform_module.git//kms?ref=feature/DD-35/kms_module"
   aliases_name            = var.aliases_name
@@ -6,20 +7,22 @@ module "Kms_module" {
   enable_key_rotation     = var.enable_key_rotation
 }
 
+// IAM Role for ECR Push
 resource "aws_iam_role" "ecr_push_role" {
-  name               = var.policy_name
+  name = var.policy_name
   assume_role_policy = jsonencode({
-    Version   = "2012-10-17",
+    Version = "2012-10-17",
     Statement = [{
-      Effect    = "Allow",
+      Effect = "Allow",
       Principal = {
         Service = "ecr.amazonaws.com"
       },
-      Action    = "sts:AssumeRole"
+      Action = "sts:AssumeRole"
     }]
   })
 }
 
+// IAM Policy for ECR Push
 resource "aws_iam_policy" "ecr_push_policy" {
   name        = var.ecr-push-policy
   description = "Policy to allow pushing images to ECR"
@@ -27,24 +30,26 @@ resource "aws_iam_policy" "ecr_push_policy" {
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
-      Effect    = "Allow",
-      Action    = [
+      Effect = "Allow",
+      Action = [
         "ecr:GetAuthorizationToken",
         "ecr:InitiateLayerUpload",
         "ecr:UploadLayerPart",
         "ecr:CompleteLayerUpload",
         "ecr:PutImage"
       ],
-      Resource  = aws_ecr_repository.ecr.arn
+      Resource = aws_ecr_repository.ecr.arn
     }]
   })
 }
 
+//IAM Role Policy Attachment
 resource "aws_iam_role_policy_attachment" "ecr_push_policy_attachment" {
   role       = aws_iam_role.ecr_push_role.name
   policy_arn = aws_iam_policy.ecr_push_policy.arn
 }
 
+// ECR Repository Creation
 resource "aws_ecr_repository" "ecr" {
   name                 = var.ecr-name
   image_tag_mutability = var.mutable
--- variable.tf
+++ variable.tf
@@ -1,46 +1,47 @@
 variable "aliases_name" {
-  type = string
-  description = "this block is for aliases name decleration"
-  default = "alias/Kms_ecr_key"
+  type        = string
+  description = "this block is for aliases name declaration"
+  default     = "alias/Kms_ecr_key"
 }
+
 variable "deleation_day" {
-  type = string
-  description = "this block is for deletion_window_in_days decleration"
-  default = 7
+  type        = string
+  description = "this block is for deletion_window_in_days declaration"
+  default     = 7
 }
 
 variable "enable_key_rotation" {
-  type = bool
-  description = "this block is for enable_key_rotation  decleration"
-  default = true
+  type        = bool
+  description = "this block is for enable_key_rotation  declaration"
+  default     = true
 }
 
 variable "policy_name" {
-  type = string
-  description = "this block is for policy_name decleration"
-  default = "ecr-push-role"
+  type        = string
+  description = "this block is for policy_name declaration"
+  default     = "ecr-push-role"
 }
 
-variable  "ecr-push-policy" {
-  type = string
-  description = "this block is for  ecr-push-policy decleration"
-  default =  "ecr-push-policy"
+variable "ecr-push-policy" {
+  type        = string
+  description = "this block is for  ecr-push-policy declaration"
+  default     = "ecr-push-policy"
 }
 
-variable  "ecr-name" {
-  type = string
-  description = "this block is for  ecr-name decleration"
-  default =  "my-ecr"
+variable "ecr-name" {
+  type        = string
+  description = "this block is for  ecr-name declaration"
+  default     = "ecr-module"
 }
 
-variable  "mutable" {
-  type = string
-  description = "this block is for mutable decleration"
-  default =  "MUTABLE"
+variable "mutable" {
+  type        = string
+  description = "this block is for mutable declaration"
+  default     = "MUTABLE"
 }
 
-variable  "encryption_type" {
-  type = string
-  description = "this block is for  encryption_type decleration"
-  default =  "KMS"
+variable "encryption_type" {
+  type        = string
+  description = "this block is for  encryption_type declaration"
+  default     = "KMS"
 }
