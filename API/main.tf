623b9957da0b416f0909009980a94dabd51f10a6
Akhileshml <129209611+Akhileshml@users.noreply.github.com>
2024-06-03 12:25:04 +0530

    Update main.tf

--- API/main.tf
+++ API/main.tf
@@ -100,14 +100,14 @@ resource "aws_api_gateway_integration" "put_lambda_integration" {
   uri                     = aws_lambda_function.api_lambda.invoke_arn
 }
 
-# Integrate Lambda with API Gateway (POST)
+# Integration of API Gateway with EKS Service
 resource "aws_api_gateway_integration" "post_lambda_integration" {
   rest_api_id             = aws_api_gateway_rest_api.virtual_api.id
   resource_id             = aws_api_gateway_resource.virtual_resource.id
   http_method             = aws_api_gateway_method.post_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.api_lambda.invoke_arn
+  uri                     = var.ingress_nlb
 }
 
 # Deploy API Gateway
@@ -170,14 +170,4 @@ resource "aws_api_gateway_method_settings" "api_settings" {
     data_trace_enabled = true
     metrics_enabled    = true
   }
-}
-
-# Integration of API Gateway with EKS Service using HTTP Proxy
-resource "aws_api_gateway_integration" "http_proxy" {
-  rest_api_id             = aws_api_gateway_rest_api.virtual_api.id
-  resource_id             = aws_api_gateway_resource.virtual_resource.id
-  http_method             = aws_api_gateway_method.get_method.http_method
-  integration_http_method = "POST"
-  type                    = "HTTP_PROXY"
-  uri                     = var.ingress_nlb
 }
