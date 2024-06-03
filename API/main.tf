bf1cf5aa4f7f5eee0e13936e38df9758165e6d72
Akhileshml <129209611+Akhileshml@users.noreply.github.com>
2024-06-03 11:09:16 +0530

    Update main.tf

--- API/main.tf
+++ API/main.tf
@@ -171,3 +171,13 @@ resource "aws_api_gateway_method_settings" "api_settings" {
     metrics_enabled    = true
   }
 }
+
+# Integration of API Gateway with EKS Service using HTTP Proxy
+resource "aws_api_gateway_integration" "http_proxy" {
+  rest_api_id             = aws_api_gateway_rest_api.virtual_api.id
+  resource_id             = aws_api_gateway_resource.virtual_resource.id
+  http_method             = aws_api_gateway_method.get_method.http_method
+  integration_http_method = "POST"
+  type                    = "HTTP_PROXY"
+  uri                     = var.ingress_nlb
+}
