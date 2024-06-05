dffb9bffbab0cd4a18a990ca1cdab9fbb7da275f
Ashwini-Kanagaraj <159413670+Ashwini-Kanagaraj@users.noreply.github.com>
2024-06-05 10:52:41 +0530

    Update main.tf

--- eks/main.tf
+++ eks/main.tf
@@ -48,24 +48,7 @@ resource "aws_iam_policy_attachment" "eks_cluster_policy_attachment" {
 # Vpc module reffering to create eks cluster
 module "vpc_module" {
   source = "git::https://github.com/Digidense/terraform_module.git//vpc?ref=feature/DD-42-VPC_module"
-  cidr              = var.cidr
-  enable_dns        = var.enable_dns
-  vpc-tag           = var.vpc-tag
-  subnet_cidr       = var.subnet_cidr
-  availability_zone = var.availability_zone
-  pub_subnet_tag1   = var.pub_subnet_tag1
-  pub_subnet_tag2   = var.pub_subnet_tag2
-  pri_subnet_tag1   = var.pri_subnet_tag1
-  pri_subnet_tag2   = var.pri_subnet_tag2
-  igw_tag           = var.igw_tag
-  pub_rt_tag        = var.pub_rt_tag
-  rt_cidr_block     = var.rt_cidr_block
-  pri_rt_tag        = var.pri_rt_tag
-  nat_gateway       = var.nat_gateway
-  sg                = var.sg
-  ingress_rules     = var.ingress_rules
-  egress_rules      = var.egress_rules
-  endpoint          = var.endpoint
+
 }
 
 # Creating the eks cluster
