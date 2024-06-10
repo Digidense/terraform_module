9869e88ea5ad30fc04edc7eb3f5759cb8c82e39f
Anishadevops <123820506+Anishadevops@users.noreply.github.com>
2024-06-10 15:55:17 +0530

    Update main.tf

--- route_53/main.tf
+++ route_53/main.tf
@@ -1,9 +1,5 @@
 # Define Route 53 zone and record
 
-module "API_module" {
-  source = "git::https://github.com/Digidense/terraform_module.git//?ref=v4.0.0"
-}
-
 resource "aws_route53_zone" "my_hosted_zone" {
   name = var.domain_name
 }
