b17919506d1ca7160232105c292eda0a61ca5507
Anishadevops <123820506+Anishadevops@users.noreply.github.com>
2024-06-10 15:56:08 +0530

    Update variable.tf

--- route_53/variable.tf
+++ route_53/variable.tf
@@ -1,5 +1,5 @@
 #Define variables
 variable "domain_name" {
   description = "The domain name you want to use"
-  default     = "stomachy-rolls.000webhostapp.com"
+  default     = "www.devopswizard.xyz"
 }
