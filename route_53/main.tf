0d4b14224de5e789169e302365816d75b4ae10a7
Anishadevops <123820506+Anishadevops@users.noreply.github.com>
2024-06-13 15:08:13 +0530

    create route_53

--- route_53/main.tf
+++ route_53/main.tf
@@ -1,14 +1,14 @@
-# Define Route 53 zone and record
-
 resource "aws_route53_zone" "my_hosted_zone" {
   name = var.domain_name
 }
 
-resource "aws_route53_record" "records" {
+resource "aws_route53_record" "alias_record" {
   zone_id = aws_route53_zone.my_hosted_zone.zone_id
   name    = "example.${var.domain_name}"
   type    = "A"
-  ttl     = 300
-  records = ["3.229.76.66"]
+  alias {
+    name                   = "a12670d490a7c4f8da6c79c340d4216c-773186114.us-east-1.elb.amazonaws.com"
+    zone_id                = "Z35SXDOTRQ7X7K" 
+    evaluate_target_health = true
+  }
 }
-
